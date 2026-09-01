c54beb6ea5d016153690685c97f6b7c24be7e2c2
Zas Dermal <zasdermal.eusuf@gmail.com>
2026-09-01 17:46:00 +0600

    invoice_number

--- module/Sales/Controllers/OrderInvoiceController.php
+++ module/Sales/Controllers/OrderInvoiceController.php
@@ -1188,9 +1188,9 @@ class OrderInvoiceController extends Controller
         ], 200);
     }
 
-    public function sale_invoice_orders(Request $request, $id)
+    public function sale_invoice_orders(Request $request, $invoice_number)
     {
-        $order_invoice = OrderInvoice::find($id);
+        $order_invoice = OrderInvoice::where('invoice_number', $invoice_number)->findOrFail();
         // $payable_amount = $order_invoice->total_amount - $order_invoice->sell_discount_amount - $order_invoice->return_amount;
         $sales_point = $order_invoice->salePoint->name . ' (' . $order_invoice->salePoint->code_number . ')';
 
